d24661bde056079c5654cbb3371991d3845d4e8c
ppby <salswnss@gmail.com>
2020-07-20 11:31:56 +0900

    Add counter module

---
+++ src/modules/counter.js
@@ -0,0 +1,33 @@
+/* 1. 액션 타입 정의 */
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+/* 2. 액션 생성 함수 만들기 */
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+/* 3. 초기 상태 밒 리듀서 함수 만들기 */
+
+const initialState = {
+  number: 0,
+};
+
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default counter;
