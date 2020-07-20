238f08f35b6d1e9e64219f739b1604c2f2876554
ppby <salswnss@gmail.com>
2020-07-20 14:40:40 +0900

    Modify counter module to redux-actions

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,3 +1,4 @@
+import { createAction, handleActions } from 'redux-actions';
 /* 1. 액션 타입 정의 */
 
 const INCREASE = 'counter/INCREASE';
@@ -5,8 +6,8 @@ const DECREASE = 'counter/DECREASE';
 
 /* 2. 액션 생성 함수 만들기 */
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 /* 3. 초기 상태 밒 리듀서 함수 만들기 */
 
@@ -14,20 +15,12 @@ const initialState = {
   number: 0,
 };
 
-const counter = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-
-    default:
-      return state;
-  }
-};
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number + 1 }),
+  },
+  initialState,
+);
 
 export default counter;
